3d2b17e409848b560059026bcd092f81bec42c95
党学义 <dangxy99@163.com>
2018-01-04 23:33:07 +0800

    时差效果

--- app/src/main/java/com/dangxy/androidpractice/behavior/FirstScrollingActivity.java
+++ app/src/main/java/com/dangxy/androidpractice/behavior/FirstScrollingActivity.java
@@ -1,6 +1,7 @@
 package com.dangxy.androidpractice.behavior;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -20,7 +21,9 @@ public class FirstScrollingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         setSupportActionBar(toolbar);
+        collapsingToolbarLayout.setTitle("title");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
